d449d669907be79924806cc9e61a6e8d87c4a51b
Sanduni Aaloka <msandunim@gmailcom>
2023-07-05 21:50:43 +0530

    feat: login validations

--- thurula/lib/views/LoginView.dart
+++ thurula/lib/views/LoginView.dart
@@ -8,59 +8,90 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // appBar: AppBar(
       //   title: const Text('Second Route'),
       // ),
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Center(
-            child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Image(
-                  image: AssetImage('assets/images/logo2.png'), height: 150),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      borderSide: BorderSide(width: 2, color: Colors.red)),
-                  labelText: 'Username',
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Center(
+              child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Image(
+                    image: AssetImage('assets/images/logo2.png'), height: 150),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                child: TextFormField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        borderSide: BorderSide(width: 2, color: Colors.red)),
+                    labelText: 'Username',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter you username';
+                    }
+                    return null;
+                  },
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      borderSide: BorderSide(width: 2, color: Colors.red)),
-                  labelText: 'Password',
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        borderSide: BorderSide(width: 2, color: Colors.red)),
+                    labelText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
                 ),
               ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.fromLTRB(120, 10, 120, 10),
-                foregroundColor: Colors.white,
-                backgroundColor: const Color.fromARGB(255, 88, 119, 161),
-                textStyle: const TextStyle(fontSize: 20, color: Colors.white),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30), // <-- Radius
+              TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(120, 10, 120, 10),
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color.fromARGB(255, 88, 119, 161),
+                  textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // <-- Radius
+                  ),
                 ),
-              ),
-              onPressed: null,
-              child: const Text('Login'),
-            )
-          ],
-        )),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // home page
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please fill input')),
+                    );
+                  }
+                },
+                child: const Text('Login'),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
