605e9ab47509e5703c71367ab3a1d319c5cffb48
adeepabandara <84633686+adeepabandara@users.noreply.github.com>
2023-08-03 00:46:40 +0530

    Add Nap View

---
+++ thurula/lib/views/addNap.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BabyCareApp());
+
+class BabyCareApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AddNapPage(),
+    );
+  }
+}
+
+class AddNapPage extends StatefulWidget {
+  @override
+  _AddNapPageState createState() => _AddNapPageState();
+}
+
+class _AddNapPageState extends State<AddNapPage> {
+  DateTime? _selectedDate;
+  TimeOfDay? _selectedStartTime;
+  TimeOfDay? _selectedEndTime;
+
+  void _showDatePicker() async {
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
+  Future<void> _showTimePicker(String type) async {
+    TimeOfDay initialTime = TimeOfDay.now();
+    if (type == 'start' && _selectedStartTime != null) {
+      initialTime = _selectedStartTime!;
+    } else if (type == 'end' && _selectedEndTime != null) {
+      initialTime = _selectedEndTime!;
+    }
+
+    final pickedTime = await showTimePicker(
+      context: context,
+      initialTime: initialTime,
+    );
+
+    if (pickedTime != null) {
+      setState(() {
+        if (type == 'start') {
+          _selectedStartTime = pickedTime;
+        } else if (type == 'end') {
+          _selectedEndTime = pickedTime;
+          // Calculate and update the difference in hours
+          final diff = pickedTime.hour - _selectedStartTime!.hour +
+              (pickedTime.minute - _selectedStartTime!.minute) / 60;
+          _selectedEndTime = pickedTime;
+          _selectedDate = DateTime(
+            _selectedDate!.year,
+            _selectedDate!.month,
+            _selectedDate!.day,
+            _selectedStartTime!.hour,
+            _selectedStartTime!.minute,
+          );
+          _selectedDate = _selectedDate!.add(Duration(hours: diff.toInt(), minutes: (diff * 60).toInt() % 60));
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Nap'),
+        backgroundColor: Colors.pink,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 16),
+            // Calendar component as a popup modal
+            TextFormField(
+              readOnly: true,
+              onTap: _showDatePicker,
+              decoration: InputDecoration(
+                labelText: 'Date',
+                labelStyle: TextStyle(color: Colors.black),
+                suffixIcon: Icon(Icons.calendar_today, color: Colors.black),
+              ),
+              controller: TextEditingController(
+                text: _selectedDate != null
+                    ? "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}"
+                    : "",
+              ),
+            ),
+
+            SizedBox(height: 16),
+            // Time selection for starting nap
+            TimeSelectionClock(
+              label: 'Start Time',
+              selectedTime: _selectedStartTime,
+              onTap: () => _showTimePicker('start'),
+            ),
+
+            SizedBox(height: 16),
+            // Time selection for ending nap
+            TimeSelectionClock(
+              label: 'End Time',
+              selectedTime: _selectedEndTime,
+              onTap: () => _showTimePicker('end'),
+            ),
+
+            SizedBox(height: 16),
+            // Display number of hours baby slept
+            TextFormField(
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'Number of Hours Slept',
+                labelStyle: TextStyle(color: Colors.black),
+                suffixIcon: Icon(Icons.access_time, color: Colors.black),
+              ),
+              controller: TextEditingController(
+                text: _selectedEndTime != null && _selectedStartTime != null
+                    ? "${(_selectedEndTime!.hour - _selectedStartTime!.hour).toString().padLeft(2, '0')}:${(_selectedEndTime!.minute - _selectedStartTime!.minute).toString().padLeft(2, '0')}"
+                    : "",
+              ),
+            ),
+
+            SizedBox(height: 16),
+            // Button to add nap details
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Implement the action to add nap details
+                },
+                style: ElevatedButton.styleFrom(primary: Colors.pink),
+                child: Text('Add Nap'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TimeSelectionClock extends StatelessWidget {
+  final String label;
+  final TimeOfDay? selectedTime;
+  final VoidCallback onTap;
+
+  TimeSelectionClock({
+    required this.label,
+    required this.selectedTime,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          readOnly: true,
+          onTap: onTap,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(color: Colors.black),
+            suffixIcon: Icon(Icons.access_time, color: Colors.black),
+          ),
+          controller: TextEditingController(
+            text: selectedTime != null
+                ? "${selectedTime!.hour.toString().padLeft(2, '0')}:${selectedTime!.minute.toString().padLeft(2, '0')}"
+                : "",
+          ),
+        ),
+      ],
+    );
+  }
+}
