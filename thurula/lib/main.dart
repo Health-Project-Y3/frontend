49683e3d607d198e5003d30db8a427d35533eab3
Pawandi-W <2020cs215@stu.ucsc.cmb.ac.lk>
2023-08-02 10:12:16 +0530

    Conflicts with main resoloved

--- thurula/lib/main.dart
+++ thurula/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thurula/views/LoginView.dart';
+import 'package:thurula/views/PregnancyTimelineView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: LoginView());
+        debugShowCheckedModeBanner: false, home: PregnancyTimelineView());
   }
 }
