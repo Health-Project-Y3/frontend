935dc41fa3468bec74aadbc7bb1ad7c479860ed9
Dushanee Gamage <dushanee2000@gmail.com>
2023-08-11 20:52:37 +0530

    add mother health tracker

--- thurula/lib/views/mother_health_tracker.dart
+++ thurula/lib/views/mother_health_tracker.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
 
 
-//
-// class MotherHealthTracker extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: MyPage(),
-//     );
-//   }
-// }
+
+
 
 class MotherHealthTracker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFDC6891), // Color for the app bar
-        title: Text("Mother Health Tracker"), // Updated app bar name
+        backgroundColor: Color(0xFFDC6891),
+        title: Text("Mother Health Tracker"),
       ),
       backgroundColor: Color(0xFFFFFFFF),
       body: Center(
@@ -29,7 +22,7 @@ class MotherHealthTracker extends StatelessWidget {
               height: 120,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.grey, // You can replace this with an image
+                color: Colors.grey,
               ),
             ),
             SizedBox(height: 16),
@@ -45,15 +38,25 @@ class MotherHealthTracker extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                DashboardCard(label: "Height", value: "184cm", isBold: true, fontSize: 20),
+                DashboardCard(
+                  label: "Height",
+                  value: "184cm",
+                  isBold: true,
+                  fontSize: 20,
+                ),
                 SizedBox(width: 16),
-                DashboardCard(label: "Weight", value: "63Kg", isBold: true, fontSize: 20),
+                DashboardCard(
+                  label: "Weight",
+                  value: "63Kg",
+                  isBold: true,
+                  fontSize: 20,
+                ),
               ],
             ),
             SizedBox(height: 16),
             Center(
               child: Container(
-                width: 200, // Two times the width of the above cards
+                width: 200,
                 child: FullWidthCard(
                   label: "Blood Pressure",
                   value: "88mmHg",
@@ -69,13 +72,26 @@ class MotherHealthTracker extends StatelessWidget {
               isBold: true,
               fontSize: 18,
             ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => RecordEntryDialog(),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFDC6891),
+                textStyle: TextStyle(color: Colors.white),
+              ),
+              child: Text('Add Record'),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
 class DashboardCard extends StatelessWidget {
   final String label;
   final String value;
@@ -156,6 +172,117 @@ class FullWidthCard extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class RecordEntryDialog extends StatefulWidget {
+  @override
+  _RecordEntryDialogState createState() => _RecordEntryDialogState();
+}
+
+class _RecordEntryDialogState extends State<RecordEntryDialog> {
+  final TextEditingController systolicController = TextEditingController();
+  final TextEditingController diastolicController = TextEditingController();
+  final TextEditingController bloodSugarController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Add Health Record'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 8),
+          Text("Blood Pressure", style: TextStyle(fontSize: 16)),
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: systolicController,
+                  decoration: InputDecoration(
+                    labelText: 'Systolic Reading',
+                    hintText: 'e.g. 120',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              SizedBox(width: 16),
+              Flexible(
+                child: TextField(
+                  controller: diastolicController,
+                  decoration: InputDecoration(
+                    labelText: 'Diastolic Reading',
+                    hintText: 'e.g. 80',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Text("Blood Sugar Level", style: TextStyle(fontSize: 16)),
+          TextField(
+            controller: bloodSugarController,
+            decoration: InputDecoration(
+              labelText: 'Blood Sugar Level',
+              hintText: 'e.g. 120 mg/dL',
+            ),
+          ),
+          SizedBox(height: 16),
+          Text("Height and Weight", style: TextStyle(fontSize: 16)),
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: heightController,
+                  decoration: InputDecoration(
+                    labelText: 'Height',
+                    hintText: 'e.g. 160 cm',
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+              Flexible(
+                child: TextField(
+                  controller: weightController,
+                  decoration: InputDecoration(
+                    labelText: 'Weight',
+                    hintText: 'e.g. 65 Kg',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Text("Date", style: TextStyle(fontSize: 16)),
+          TextField(
+            controller: dateController,
+            decoration: InputDecoration(
+              labelText: 'Date (yyyy-mm-dd)',
+              hintText: 'e.g. 2023-08-15',
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            // Add record logic
+            Navigator.pop(context);
+          },
+          child: Text('Add'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Cancel'),
+        ),
+      ],
     );
   }
 }
