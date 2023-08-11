36719135f808deb339afee801571e13998306d5f
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-08-11 16:32:58 +0530

    length chart for nbqsa

--- thurula/lib/views/length_chart_view.dart
+++ thurula/lib/views/length_chart_view.dart
@@ -12,14 +12,13 @@ class LengthChartView extends StatefulWidget {
 }
 
 Future<List<List<Point>>> loadData(gender, {id = ""}) async {
-
   List<List<Point>> allDataLists = [];
 
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 10));
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 25));
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 50));
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 75));
-  allDataLists.add(await BabyLengthService().getReferenceLength(gender,90));
+  allDataLists.add(await BabyLengthService().getReferenceLength(gender, 90));
   if (id != "") {
     allDataLists.add(await BabyLengthService().getBabyLength(id));
   }
@@ -31,50 +30,48 @@ class _LengthChartViewState extends State<LengthChartView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 244, 200, 200),
         appBar: AppBar(
-          title: const Text('Length Chart'),
+          backgroundColor: Color.fromARGB(255, 244, 200, 200),
+          title: const Text(
+            'Length Chart',
+            style: TextStyle(
+              color: Color.fromARGB(255, 88, 119, 161),
+            ),
+          ),
         ),
         //use padding to add space between the widgets
         body: Center(
             child: Column(children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: Text(
-                  'Length Chart',
-                  style: TextStyle(fontSize: 30),
+          Padding(
+            //padding more on the left and right and less on the top and bottom
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: SizedBox(
+                width: 400,
+                height: 650,
+                child: FutureBuilder(
+                  //TODO change the temporary id to the actual id
+                  future: loadData("male"),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      print(snapshot.data);
+                      print(snapshot.data.runtimeType);
+                      return Container(
+                          decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: BabyLengthChartWidget(snapshot.data));
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
                 ),
               ),
-              Padding(
-                //padding more on the left and right and less on the top and bottom
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    //light pink
-                      color:  Color(0xFFDAB7B7),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  child: SizedBox(
-                    width: 400,
-                    height: 600,
-                    child: FutureBuilder(
-                      //TODO change the temporary id to the actual id
-                      future: loadData("male"),
-                      builder:
-                          (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.hasData) {
-                          print(snapshot.data);
-                          print(snapshot.data.runtimeType);
-                          return Container(
-                              decoration:
-                              const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20))),
-                              child: BabyLengthChartWidget(snapshot.data));
-                        } else {
-                          return const CircularProgressIndicator();
-                        }
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ])));
+            ),
+          ),
+        ])));
   }
 }
--- thurula/lib/views/widgets/graphs/baby_length_chart_widget.dart
+++ thurula/lib/views/widgets/graphs/baby_length_chart_widget.dart
@@ -24,122 +24,70 @@ class _BabyLengthChartWidgetState extends State<BabyLengthChartWidget> {
       scrollDirection: Axis.horizontal,
       child: SizedBox(
         width: 1000,
-        child: LineChart(
-          LineChartData(
-            minX: minX,
-            maxX: maxX,
-            minY: minY,
-            maxY: maxY,
-            gridData: const FlGridData(
-              show: true,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: LineChart(
+            LineChartData(
+              minX: minX,
+              maxX: maxX,
+              minY: minY,
+              maxY: maxY,
+              gridData: const FlGridData(
+                show: false,
+              ),
+              titlesData: const FlTitlesData(
+                leftTitles: AxisTitles(
+                    axisNameWidget: Text('Length(cm)'),
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 40,
+                      interval: 5,
+                    )),
+                bottomTitles: AxisTitles(
+                    axisNameWidget: Text('Weeks'),
+                    sideTitles: SideTitles(
+                        showTitles: true, reservedSize: 40)),
+                topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              ),
+              lineBarsData: [
+                LineChartBarData(
+                  spots: widget.dataList[0].map((point) {
+                    return FlSpot(point.x, point.y);
+                  }).toList(),
+
+                  color: Colors.red,
+                ),
+                LineChartBarData(
+                  spots: widget.dataList[1].map((point) {
+                    return FlSpot(point.x, point.y);
+                  }).toList(),
+                  isCurved: false,
+                  color: Colors.yellow[800],
+                ),
+                LineChartBarData(
+                  spots: widget.dataList[2].map((point) {
+                    return FlSpot(point.x, point.y);
+                  }).toList(),
+                  isCurved: false,
+                  color: Colors.green,
+                ),
+                LineChartBarData(
+                  spots: widget.dataList[3].map((point) {
+                    return FlSpot(point.x, point.y);
+                  }).toList(),
+                  isCurved: false,
+                  color: Colors.yellow[800],
+                ),
+                LineChartBarData(
+                  spots: widget.dataList[4].map((point) {
+                    return FlSpot(point.x, point.y);
+                  }).toList(),
+                  isCurved: false,
+                  color: Colors.red,
+                )
+              ],
             ),
-            titlesData: const FlTitlesData(
-              leftTitles: AxisTitles(
-                  axisNameWidget: Text('Length(cm)'),
-                  sideTitles: SideTitles(
-                    showTitles: true,
-                    reservedSize: 40,
-                    interval: 5,
-                  )),
-              bottomTitles: AxisTitles(
-                  axisNameWidget: Text('Weeks'),
-                  sideTitles: SideTitles(
-                      showTitles: true, reservedSize: 40)),
-              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            ),
-            lineBarsData: [
-              LineChartBarData(
-                spots: widget.dataList[0].map((point) {
-                  return FlSpot(point.x, point.y);
-                }).toList(),
-                isCurved: true,
-                color: Colors.red,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (FlSpot spot, double? percent,
-                      LineChartBarData barData, int index) {
-                    return FlDotCirclePainter(
-                      strokeWidth: 1.5,
-                      radius: 1.5,
-                      color: Colors.black,
-                    );
-                  },
-                ),
-              ),
-              LineChartBarData(
-                spots: widget.dataList[1].map((point) {
-                  return FlSpot(point.x, point.y);
-                }).toList(),
-                isCurved: true,
-                color: Colors.yellow,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (FlSpot spot, double? percent,
-                      LineChartBarData barData, int index) {
-                    return FlDotCirclePainter(
-                      strokeWidth: 1.5,
-                      radius: 1.5,
-                      color: Colors.black,
-                    );
-                  },
-                ),
-              ),
-              LineChartBarData(
-                spots: widget.dataList[2].map((point) {
-                  return FlSpot(point.x, point.y);
-                }).toList(),
-                isCurved: true,
-                color: Colors.green,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (FlSpot spot, double? percent,
-                      LineChartBarData barData, int index) {
-                    return FlDotCirclePainter(
-                      strokeWidth: 1.5,
-                      radius: 1.5,
-                      color: Colors.black,
-                    );
-                  },
-                ),
-              ),
-              LineChartBarData(
-                spots: widget.dataList[3].map((point) {
-                  return FlSpot(point.x, point.y);
-                }).toList(),
-                isCurved: true,
-                color: Colors.yellow,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (FlSpot spot, double? percent,
-                      LineChartBarData barData, int index) {
-                    return FlDotCirclePainter(
-                      strokeWidth: 1.5,
-                      radius: 1.5,
-                      color: Colors.black,
-                    );
-                  },
-                ),
-              ),
-              LineChartBarData(
-                spots: widget.dataList[4].map((point) {
-                  return FlSpot(point.x, point.y);
-                }).toList(),
-                isCurved: true,
-                color: Colors.red,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (FlSpot spot, double? percent,
-                      LineChartBarData barData, int index) {
-                    return FlDotCirclePainter(
-                      strokeWidth: 1.5,
-                      radius: 1.5,
-                      color: Colors.black,
-                    );
-                  },
-                ),
-              )
-            ],
           ),
         ),
       ),
