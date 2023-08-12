f6fb9227cf166e12ebc38561df832b3929cf68bc
venudi <venudidisara@gmail.com>
2023-08-12 11:19:23 +0530

    pregnancy vaccination

--- thurula/lib/views/child_vaccination_tracker_view.dart
+++ thurula/lib/views/child_vaccination_tracker_view.dart
@@ -55,7 +55,7 @@ class _ChildVaccinationTrackerView extends State<ChildVaccinationTrackerView> {
               bottom: PreferredSize(
                 preferredSize: const Size.fromHeight(50.0), // height of  TabBar
                 child: Container(
-                  color: Color.fromARGB(255, 220, 104, 145),
+                  color: const Color.fromARGB(255, 220, 104, 145),
                   child: const TabBar(
                     tabs: [
                       Tab(
