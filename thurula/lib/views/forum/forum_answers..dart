cf135d4ed7c7e3c4f5938c08382ad524975eea2e
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-03 12:52:46 +0530

    Basic Pages for Forum with buggy infinite scroll

--- thurula/lib/views/forum/forum_answers..dart
+++ thurula/lib/views/forum/forum_answers..dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thurula/services/forum_service.dart';
+import 'package:thurula/views/forum/forum_answer_widget.dart';
 import '../../models/forum_question_model.dart';
 import 'dart:async';
 
@@ -77,37 +78,8 @@ class _ForumAnswersViewState extends State<ForumAnswersView> {
                   ),
                 ),
                 // Display answers here
-                ...?question.answers?.map((answer) => Card(
-                  margin: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(answer.answer ?? ''),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Answered by: ${'${answer.authorFirstName} ${answer.authorLastName}'}'),
-                        Text('Date Answered: ${ForumQuestion.getDate(answer.date!)}'),
-                        Row(
-                          children: [
-                            const Icon(Icons.thumb_up),
-                            const SizedBox(width: 4),
-                            // Add some spacing
-                            Text('${question.upvotes}'),
-                            // Replace 'upvotes' with your property
-                            const SizedBox(width: 16),
-                            // Add spacing between the icons
-                            const Icon(Icons.thumb_down),
-                            const SizedBox(width: 4),
-                            // Add some spacing
-                            Text('${question.downvotes}'),
-                            // Replace 'downvotes' with your property
-                          ],
-                        ),
 
-                        // Add more answer details as needed
-                      ],
-                    ),
-                  ),
-                )).toList(),
+                ... question.answers?.map((answer) => ForumAnswerCard(answer: answer)).toList() ?? [],
               ],
             );
           }
--- thurula/lib/views/forum/forum_home.dart
+++ thurula/lib/views/forum/forum_home.dart
@@ -1,6 +1,7 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:thurula/services/forum_service.dart';
-import 'package:thurula/views/forum/forum_answers..dart';
+import 'package:thurula/views/forum/forum_question_widget.dart';
 import '../../models/forum_question_model.dart';
 import 'dart:async';
 
@@ -13,12 +14,60 @@ class ForumHomeView extends StatefulWidget {
 
 class _ForumHomeViewState extends State<ForumHomeView> {
   Future<List<ForumQuestion>>? _futureQuestions;
+  bool _reachedEndOfData = false;
+  var pageNumber = 1;
+  final _pageSize = 10;
+  final ScrollController _scrollController = ScrollController();
+  List<ForumQuestion> existingQuestions = [];
+
+  void _scrollListener() {
+    if (!_reachedEndOfData &&
+        _scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent) {
+      // User has reached the end of the list, load more questions
+      _loadMoreQuestions();
+    }
+  }
+
+  void _loadMoreQuestions() {
+    // Fetch the next page of questions and append them to the existing list
+    final nextPage = _futureQuestions!.then((existingQuestions) {
+      final nextPageNumber = (existingQuestions.length / _pageSize).ceil() + 1;
+      log("Next PageNumber : $nextPageNumber");
+      return ForumService.getRecentQuestions(nextPageNumber, _pageSize);
+    });
+
+    nextPage.then((nextPageQuestions) {
+      setState(() {
+        if (nextPageQuestions.isEmpty) {
+          // No more data available
+          _reachedEndOfData = true;
+        } else {
+          // Append the new questions to the existing list
+          existingQuestions.addAll(nextPageQuestions);
+          _reachedEndOfData = false;
+        }
+        _futureQuestions = Future.value(existingQuestions);
+      });
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     _futureQuestions = ForumService.getRecentQuestions(
-        1, 10); // Change page and pageSize as needed
+        1, _pageSize); // Change page and pageSize as needed
+    //also add to existingQuestions
+    _futureQuestions!.then((questions) {
+      existingQuestions.addAll(questions);
+    });
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -27,75 +76,51 @@ class _ForumHomeViewState extends State<ForumHomeView> {
       appBar: AppBar(
         title: const Text('Recent Forum Questions'),
       ),
-      body: FutureBuilder<List<ForumQuestion>>(
-        future: _futureQuestions,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text('No recent questions available.'),
-            );
-          } else {
-            final questions = snapshot.data!;
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder<List<ForumQuestion>>(
+              future: _futureQuestions,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No recent questions available.'),
+                  );
+                } else {
+                  final questions = snapshot.data!;
 
-            return ListView.builder(
-              itemCount: questions.length,
-              itemBuilder: (context, index) {
-                final question = questions[index];
-
-                return GestureDetector(
-                    child: Card(
-                      margin: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        title: Text(question.question ?? ''),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                'Posted by: ${'${question.authorFirstName} ${question.authorLastName}'}'),
-                            Text(
-                                'Date Posted: ${ForumQuestion.getDate(question.date!)}'),
-                            Text(question.description ?? ''),
-                            Row(
-                              children: [
-                                const Icon(Icons.thumb_up),
-                                const SizedBox(width: 4),
-                                // Add some spacing
-                                Text('${question.upvotes}'),
-                                // Replace 'upvotes' with your property
-                                const SizedBox(width: 16),
-                                // Add spacing between the icons
-                                const Icon(Icons.thumb_down),
-                                const SizedBox(width: 4),
-                                // Add some spacing
-                                Text('${question.downvotes}'),
-                                // Replace 'downvotes' with your property
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              ForumAnswersView(questionId: question.id!),
-                        ),
-                      );
-                    });
+                  return ListView.builder(
+                    controller: _scrollController,
+                    itemCount: questions.length + (_reachedEndOfData ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      final question = questions[index];
+                      return ForumQuestionCard(question: question);
+                    },
+                  );
+                }
               },
-            );
-          }
-        },
+            ),
+          ),
+          if (_reachedEndOfData)
+            // Optionally, you can display a button or other UI elements here
+            // to allow the user to reload the list or perform some other action
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle the action to reload or do something else
+                },
+                child: const Text('Reload Questions'),
+              ),
+            ),
+        ],
       ),
     );
   }
