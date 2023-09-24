b7cb10acdf4cf38bd9df0d9270db2880b4a32490
Dushanee Gamage <dushanee2000@gmail.com>
2023-09-24 22:02:26 +0530

    Update diaper_change.dart

--- thurula/lib/views/childcare/diaper_change.dart
+++ thurula/lib/views/childcare/diaper_change.dart
@@ -1,255 +1,260 @@
 import 'package:flutter/material.dart';
+import 'package:thurula/services/diapers_service.dart';
+import 'package:thurula/models/diapertimes_model.dart';
 
 class DiaperChange extends StatefulWidget {
-  const DiaperChange({super.key});
-
   @override
-  _DiaperChange createState() => _DiaperChange();
+  _DiaperChangeState createState() => _DiaperChangeState();
 }
 
-class _DiaperChange extends State<DiaperChange> {
+class _DiaperChangeState extends State<DiaperChange> {
+  Future<List<DiaperTimes>>? _diaperRecordsFuture;
+  List<DiaperTimes> diaperRecords = [];
+  DateTime? newDateTime; // Added for selecting date and time
+
+  @override
+  void initState() {
+    super.initState();
+    _diaperRecordsFuture = DiaperService.getBabyDiapers('64b01605b55b765169e1c9b6');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Diaper Change',
-          style: TextStyle(
-            color: Colors.white, // Title color
-          ),
-        ),
+        title: Text('Diaper Change'),
         backgroundColor: const Color.fromARGB(255, 220, 104, 145),
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white, // Back icon color
-          ),
-          onPressed: () {
-            // Handle back button press here
-            Navigator.pop(context); // Navigate back
-          },
-        ),
       ),
+      body: FutureBuilder<List<DiaperTimes>>(
+        future: _diaperRecordsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No diaper change records found.'));
+          } else {
+            // Data has been loaded successfully.
+            diaperRecords = snapshot.data!;
 
-
-
-      body: SingleChildScrollView(
-        // Wrap the Column with SingleChildScrollView
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(16),
-              child: const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage(
-                    'assets/images/menu-icons/diaper.png'), // Replace with your circular image asset
-              ),
-            ),
-            Card(
-              elevation: 4,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: DiaperChangeEntryForm(),
-              ),
-            ),
-            SizedBox(height: 16),
-            Card(
-              elevation: 4,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: DiaperChangeDetails(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DiaperChangeEntryForm extends StatefulWidget {
-  @override
-  _DiaperChangeEntryFormState createState() => _DiaperChangeEntryFormState();
-}
-
-class _DiaperChangeEntryFormState extends State<DiaperChangeEntryForm> {
-  final TextEditingController _dateController = TextEditingController();
-  final TextEditingController _timeController = TextEditingController();
-
-  @override
-  void dispose() {
-    _dateController.dispose();
-    _timeController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _dateController,
-          decoration: InputDecoration(
-            labelText: 'Date (yyyy-mm-dd)',
-            labelStyle: TextStyle(color: Colors.grey), // Adjust the color
-          ),
-        ),
-        TextField(
-          controller: _timeController,
-          decoration: InputDecoration(
-            labelText: 'Time (hh:mm)',
-            labelStyle: TextStyle(color: Colors.grey), // Adjust the color
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            // Handle button press
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 220, 104, 145),
-          ),
-          child: Text('Add Diaper Change'),
-        ),
-      ],
-    );
-  }
-}
-
-class DiaperChangeEntry {
-  final DateTime dateTime;
-
-  DiaperChangeEntry(this.dateTime);
-}
-
-class DiaperChangeDetails extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Stack(
-        children: [
-          Positioned(
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: const Center(
-                child: Text(
-                  'Diaper Change Details',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 220, 104, 145), // Text color for the header
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            // Create a ListView to display diaper change records
+            return ListView.builder(
+              itemCount: diaperRecords.length,
+              itemBuilder: (context, index) {
+                DiaperTimes record = diaperRecords[index];
+                return Card(
+                  elevation: 3,
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Time: ${record.time?.toString() ?? ''}'),
+                        SizedBox(height: 8),
+                        Text('Diaper Type: ${record.diaperType ?? ''}'),
+                        SizedBox(height: 8),
+                        Text('Diaper Notes: ${record.diaperNotes ?? ''}'),
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {
+                            _showEditDiaperDialog(context, record);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _deleteDiaperRecord(record.id ?? '');
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ),
-          DataTable(
-            dividerThickness: 0,
-            headingRowHeight: 60, // Adjust the header row height as needed
-            columns: const [
-              DataColumn(
-                label: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text('Time'),
-                ),
-              ),
-              DataColumn(
-                label: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text('Date'),
-                ),
-              ),
-              DataColumn(
-                label: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text('Edit'),
-                ),
-              ),
-              DataColumn(
-                label: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text('Delete'),
-                ),
-              ),
-            ],
-            rows: [
-              DataRow(cells: [
-                const DataCell(Text('09:00')),
-                const DataCell(Text('2023-08-10')),
-                DataCell(
-                    SizedBox(
-                      width: 30, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {
-                        },
-                      ),
-                    ),
-                ),
-                //
-                DataCell(
-                    SizedBox(
-                      width: 20, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {},
-                      ),
-                    ),
-                ),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('13:30')),
-                DataCell(Text('2023-08-10')),
-                DataCell(
-                SizedBox(
-                      width: 30, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    ),
-                ),
-                DataCell(
-                    SizedBox(
-                      width: 20, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {},
-                      ),
-                    ),
-                ),
-              ]),
-              DataRow(cells: [
-                const DataCell(Text('10:45')),
-                const DataCell(Text('2023-08-11')),
-                DataCell(
-                SizedBox(
-                      width: 30, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    ),
-                ),
-                DataCell(
-                    SizedBox(
-                      width: 20, // Adjust the width as needed
-                      child: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {},
-                      ),
-                    ),
-                ),
-              ]),
-              // ... add more rows
-            ],
-          ),
-        ],
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddDiaperDialog(context);
+        },
+        child: Icon(Icons.add),
+        backgroundColor: const Color.fromARGB(255, 220, 104, 145),
       ),
     );
+  }
+
+  Future<void> _showAddDiaperDialog(BuildContext context) async {
+    TextEditingController diaperNotesController = TextEditingController();
+    String selectedDiaperType = 'Nothing';
+    DateTime? selectedDateTime = DateTime.now();
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Add Diaper Record'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () => _selectDateTime(context),
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'Date and Time',
+                          labelStyle: TextStyle(color: Colors.grey),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              selectedDateTime != null
+                                  ? "${selectedDateTime.toLocal()}".split(' ')[0]
+                                  : "Select Date and Time",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Text(
+                              selectedDateTime != null
+                                  ? "${selectedDateTime.toLocal()}".split(' ')[1]
+                                  : "",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Icon(
+                              Icons.calendar_today,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: 'Diaper Type',
+                        labelStyle: TextStyle(color: Colors.grey),
+                      ),
+                      child: DropdownButtonFormField<String>(
+                        value: selectedDiaperType,
+                        onChanged: (newValue) {
+                          setState(() {
+                            selectedDiaperType = newValue!;
+                          });
+                        },
+                        items: ['Wet', 'Dry', 'Nothing'].map((type) {
+                          return DropdownMenuItem<String>(
+                            value: type,
+                            child: Text(type),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    TextField(
+                      controller: diaperNotesController,
+                      decoration: InputDecoration(labelText: 'Diaper Notes'),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('Save'),
+                  onPressed: () async {
+                    if (selectedDateTime == null ||
+                        selectedDiaperType == null ||
+                        selectedDiaperType.isEmpty) {
+                      // Display an error message or prevent submission
+                      return;
+                    }
+
+                    final newRecord = DiaperTimes(
+                      diaperNotes: diaperNotesController.text,
+                      diaperType: selectedDiaperType,
+                      time: selectedDateTime,
+                    );
+
+                    try {
+                      final DiaperTimes? addedRecord =
+                      await DiaperService.createDiaper(newRecord);
+
+                      if (addedRecord != null) {
+                        setState(() {
+                          diaperRecords.add(addedRecord);
+                        });
+
+                        // Close the dialog
+                        Navigator.of(context).pop();
+                      } else {
+                        print('Failed to add diaper record: Unexpected API response');
+                      }
+                    } catch (e) {
+                      print('Failed to add diaper record: $e');
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  // Define the edit diaper dialog here
+  void _showEditDiaperDialog(BuildContext context, DiaperTimes record) {
+    // Implement your edit diaper dialog logic here
+  }
+
+  void _deleteDiaperRecord(String id) async {
+    try {
+      await DiaperService.deleteDiaper(id);
+      setState(() {
+        diaperRecords.removeWhere((record) => record.id == id);
+      });
+    } catch (e) {
+      print('Failed to delete diaper record: $e');
+    }
+  }
+
+  void _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDateTime = await showDatePicker(
+      context: context,
+      initialDate: newDateTime ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDateTime != null) {
+      final TimeOfDay? pickedTimeOfDay = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(newDateTime ?? DateTime.now()),
+      );
+
+      if (pickedTimeOfDay != null) {
+        setState(() {
+          newDateTime = DateTime(
+            pickedDateTime.year,
+            pickedDateTime.month,
+            pickedDateTime.day,
+            pickedTimeOfDay.hour,
+            pickedTimeOfDay.minute,
+          );
+        });
+      }
+    }
   }
 }
