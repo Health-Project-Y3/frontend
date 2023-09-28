dd2673cdd08c437afdcfacfc030189e741d525eb
Pawandi-W <2020cs215@stu.ucsc.cmb.ac.lk>
2023-09-28 19:39:25 +0530

    Baby Growth Chart  - Inputs,Updates

--- thurula/lib/views/childcare/length_chart_view.dart
+++ thurula/lib/views/childcare/length_chart_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:thurula/services/baby_length_service.dart';
 import 'package:thurula/views/widgets/graphs/baby_length_chart_widget.dart';
-
 import '../widgets/graphs/point_widget.dart';
+
+int lengthAddedMonths = 0;
+
+const babyid = "64a9cb10ec5c9834ff73fc36";
 
 class LengthChartView extends StatefulWidget {
   const LengthChartView({super.key});
@@ -19,88 +22,311 @@ Future<List<List<Point>>> loadData(gender, {id = ""}) async {
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 50));
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 75));
   allDataLists.add(await BabyLengthService().getReferenceLength(gender, 90));
+
   if (id != "") {
-    allDataLists.add(await BabyLengthService().getBabyLength(id));
+    final babyData = await BabyLengthService().getBabyLength(id);
+    final modifiedBabyData = babyData
+        .where((point) => point.y != -1.0) // Filter out points with y = -1.0
+        .toList();
+
+    allDataLists.add(modifiedBabyData);
+    lengthAddedMonths = modifiedBabyData.length;
   }
 
   return allDataLists;
 }
 
+// Define ValueNotifier here
+ValueNotifier<int> refreshCounter = ValueNotifier<int>(0);
+
 class _LengthChartViewState extends State<LengthChartView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text('Length Chart'),
-            backgroundColor: const Color.fromARGB(255, 220, 104, 145)),
-        // use padding to add space between the widgets
-        body: Center(
-            child: Column(children: <Widget>[
-          Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 20, 10),
+      appBar: AppBar(
+        title: const Text('Length Chart'),
+        backgroundColor: const Color.fromARGB(255, 220, 104, 145),
+      ),
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.fromLTRB(40, 15, 40, 10),
               child: Row(children: [
-                const SizedBox(
-                    child: RotatedBox(
-                        quarterTurns: -1,
-                        child: Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              'Length (cm)',
-                              style: TextStyle(fontSize: 16),
-                              // overflow: TextOverflow.ellipsis,
-                            )))),
-                Expanded(
-                  child: Stack(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 20, left: 10),
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFFED7E3),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: SizedBox(
-                          width: 400,
-                          height: 600,
-                          child: FutureBuilder(
-                            //TODO change the temporary id to the actual id
-                            future: loadData("male"),
-                            builder:
-                                (BuildContext context, AsyncSnapshot snapshot) {
-                              if (snapshot.hasData) {
-                                // print(snapshot.data);
-                                // print(snapshot.data.runtimeType);
-                                return Container(
-                                    decoration: const BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20))),
-                                    child:
-                                        BabyLengthChartWidget(snapshot.data));
-                              } else {
-                                return const SizedBox(
-                                  height: 10.0,
-                                  width: 10.0,
-                                  child: Center(
-                                      child: CircularProgressIndicator(
-                                    //   color
-                                    color: Color.fromARGB(255, 220, 104, 145),
-                                  )),
-                                );
-                              }
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
+                const Text(
+                  'Baby Growth Chart',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ])),
-          const Positioned(
+                const SizedBox(width: 25),
+                GestureDetector(
+                  onTap: () {
+                    showAdd(context);
+                  },
+                  child: const Icon(
+                    Icons.add_circle,
+                    color: Color.fromARGB(255, 220, 104, 145),
+                  ),
+                ),
+                const SizedBox(width: 25),
+                GestureDetector(
+                  onTap: () {
+                    showUpdate(context);
+                  },
+                  child: const Icon(
+                    Icons.edit_outlined,
+                    color: Color.fromARGB(255, 220, 104, 145),
+                  ),
+                ),
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 20, 10),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    child: RotatedBox(
+                      quarterTurns: -1,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          'Length (cm)',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(top: 15, left: 10),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: SizedBox(
+                            width: 400,
+                            height: 500,
+                            child: ValueListenableBuilder<int>(
+                              valueListenable: refreshCounter,
+                              builder: (BuildContext context, int value,
+                                  Widget? child) {
+                                return FutureBuilder(
+                                  future: loadData("male", id: babyid),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Container(
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(20)),
+                                        ),
+                                        child: BabyLengthChartWidget(
+                                            snapshot.data),
+                                      );
+                                    } else {
+                                      return const SizedBox(
+                                        height: 10.0,
+                                        width: 10.0,
+                                        child: Center(
+                                          child: CircularProgressIndicator(
+                                            color: Color.fromARGB(
+                                                255, 220, 104, 145),
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Positioned(
               bottom: 0,
               left: 0,
               right: 0,
               child: Center(
-                child: Text('Age (weeks)', style: TextStyle(fontSize: 16)),
-              ))
-        ])));
+                child: Text('Age (months)', style: TextStyle(fontSize: 16)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+// Add Reading
+class AddReadingDialog extends StatefulWidget {
+  @override
+  _AddReadingDialogState createState() => _AddReadingDialogState();
+}
+
+class _AddReadingDialogState extends State<AddReadingDialog> {
+  int selectedMonth = lengthAddedMonths; // Default selected month
+  double? length; // Variable to store the entered length
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Add New Reading'),
+      contentPadding:
+          const EdgeInsets.fromLTRB(20, 20, 20, 0), // Adjust padding as needed
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          DropdownButton<int>(
+            value: selectedMonth,
+            onChanged: (int? newValue) {
+              setState(() {
+                selectedMonth = newValue!;
+              });
+            },
+            items: List.generate(
+              25 - lengthAddedMonths,
+              (index) => DropdownMenuItem<int>(
+                value: index + lengthAddedMonths,
+                child: Text('Month ${index + lengthAddedMonths}'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10), // Adjust the height as needed
+          TextField(
+            decoration: const InputDecoration(labelText: 'Length (cm)'),
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              // Parse the value to a double and store it in the length variable
+              length = double.tryParse(value);
+            },
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: const Text('Close'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (length != null) {
+              BabyLengthService().addBabyLength(babyid, selectedMonth, length!);
+              Navigator.of(context).pop(); // Close the dialog
+              refreshCounter.value++; // Trigger UI update
+            } else {
+              print("error");
+            }
+          },
+          child: const Text('Add'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 88, 119, 161),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void showAdd(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AddReadingDialog(); // Use the Stateful dialog
+    },
+  );
+}
+
+// Update Reading
+class UpdateReadingDialog extends StatefulWidget {
+  @override
+  _UpdatReadingDialogState createState() => _UpdatReadingDialogState();
+}
+
+class _UpdatReadingDialogState extends State<UpdateReadingDialog> {
+  int selectedMonth = 0; // Default selected month
+  double? length; // Variable to store the entered length
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Update Reading'),
+      contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          DropdownButton<int>(
+            value: selectedMonth,
+            onChanged: (int? newValue) {
+              setState(() {
+                selectedMonth = newValue!;
+              });
+            },
+            items: List.generate(
+              lengthAddedMonths - 1,
+              (index) => DropdownMenuItem<int>(
+                value: index,
+                child: Text('Month ${index + 1}'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: const InputDecoration(labelText: 'Length (cm)'),
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              // Parse the value to a double and store it in the length variable
+              length = double.tryParse(value);
+            },
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: const Text('Close'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (length != null) {
+              BabyLengthService()
+                  .editBabyLength(babyid, selectedMonth + 1, length!);
+              Navigator.of(context).pop(); // Close the dialog
+              refreshCounter.value++; // Trigger UI update
+            } else {
+              print("error");
+            }
+          },
+          child: const Text('Update'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 88, 119, 161),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void showUpdate(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return UpdateReadingDialog(); // Use the Stateful dialog
+    },
+  );
 }
