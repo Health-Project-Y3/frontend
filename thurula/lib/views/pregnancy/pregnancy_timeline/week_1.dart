74bb9a6c1178642c23f6074963647813cb7960e6
venudi <venudidisara@gmail.com>
2023-10-01 00:48:14 +0530

    Pregnancy timeline week 1 UI update

--- thurula/lib/views/pregnancy/pregnancy_timeline/week_1.dart
+++ thurula/lib/views/pregnancy/pregnancy_timeline/week_1.dart
@@ -8,141 +8,272 @@ class Week1View extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 227, 227, 227),
-      body: Column(
+      // #f7f7f7
+      backgroundColor: const Color.fromARGB(255, 247, 247, 247),
+      body: SingleChildScrollView(
+          child: Column(
         children: [
           const Center(
               child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Text("First trimester - Week 1",
+            padding: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: 20.0, bottom: 0.0),
+            child: Text("First Trimester - Week 1",
                 style: TextStyle(
                   color: Color.fromARGB(255, 220, 104, 145),
                   fontFamily: 'Inter',
-                  fontSize: 26,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
                 )),
           )),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Your Baby’s Development',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 88, 119, 161),
-                  fontFamily: 'Inter',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(
-                    10.0), // change this to the desired amount of padding
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(80.0),
-                  child: Image.asset('assets/images/fetus-size/1.jpg',
-                      height: 100),
-                ),
-              ),
-              const Expanded(
-                child: Text(
-                  'Surprise! You are not actually pregnant during your first week of pregnancy! Your due date is calculated from the first day of your last period.',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 88, 119, 161),
-                    fontSize: 14.0,
-                    height: 1.5,
-                  ), // change this to the desired text color
-                ),
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 13.0, 20.0, 0.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Your Body',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 88, 119, 161),
-                  fontFamily: 'Inter',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(
-                20.0), // change this to the desired amount of padding
-            child: Text(
-              "Since doctors calculate your due date from the first day of your last menstrual period, week one begins with the start of the last period you have before you conceive. That's to say that you're not technically pregnant yet.\nYou can, however, expect to experience your typical menstrual symptoms including bleeding, cramping, sore breasts, mood swings, etc.",
-              style: TextStyle(
-                color: Color.fromARGB(255, 88, 119, 161),
-                fontSize: 14.0,
-                height: 1.5,
-              ), // change this to the desired text color
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 13.0, 20.0, 0.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Tips for You This Week',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 88, 119, 161),
-                  fontFamily: 'Inter',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          ListView(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            children: const [
-              Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-                child: Text(
-                  "Fill up on folic acid.",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 88, 119, 161),
-                    fontSize: 14.0,
-                    height: 1.5,
+          Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15.0, bottom: 5),
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 0.5,
+                        blurRadius: 3,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: 15, bottom: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Row(
+                          children: [
+                            // bullet point
+                            Icon(
+                              Icons.rectangle_rounded,
+                              size: 20,
+                              color: Color.fromARGB(255, 220, 104, 145),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Your Baby\'s Development',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 88, 119, 161),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            //   wrapped text
+                            const Expanded(
+                              child: Text(
+                                'Surprise! You are not actually pregnant during your first week of pregnancy! Your due date is calculated from the first day of your last period.',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                            // bullet point
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5.0),
+                                child: Image.asset(
+                                    'assets/images/fetus-size/1.jpg',
+                                    height: 100),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ))),
+
+          // Your body
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 0),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0.5,
+                      blurRadius: 3,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-                child: Text(
-                  "Spot early pregnancy signs.",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 88, 119, 161),
-                    fontSize: 14.0,
-                    height: 1.5,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                      left: 20.0, right: 20.0, top: 15, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          // bullet point
+                          Icon(
+                            Icons.rectangle_rounded,
+                            size: 20,
+                            color: Color.fromARGB(255, 220, 104, 145),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            'Your Body',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 88, 119, 161),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Since doctors calculate your due date from the first day of your last menstrual period, week one begins with the start of the last period you have before you conceive. That's to say that you're not technically pregnant yet.\nYou can, however, expect to experience your typical menstrual symptoms including bleeding, cramping, sore breasts, mood swings, etc.",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          height: 1.5,
+                        ),
+                      ),
+                    ],
                   ),
+                )),
+          ),
+
+          // Weekly tips
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 15.0, right: 15.0, top: 5, bottom: 0),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0.5,
+                      blurRadius: 3,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-                child: Text(
-                  "Shop for pregnancy tests.",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 88, 119, 161),
-                    fontSize: 14.0,
-                    height: 1.5,
+
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                      left: 20.0, right: 20.0, top: 15, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'TIPS FOR YOU THIS WEEK',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 220, 104, 145),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          // bullet point
+                          Icon(
+                            Icons.tips_and_updates,
+                            size: 22,
+                            color: Color.fromARGB(255, 220, 104, 145),
+                          ),
+                          SizedBox.square(
+                            dimension: 10,
+                          ),
+                          //   wrapped text
+                          Text(
+                            'Spot early pregnancy signs.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              height: 1.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          // bullet point
+                          Icon(
+                            Icons.tips_and_updates,
+                            size: 22,
+                            color: Color.fromARGB(255, 220, 104, 145),
+                          ),
+                          SizedBox.square(
+                            dimension: 10,
+                          ),
+                          //   wrapped text
+                          Text(
+                            'Shop for pregnancy tests.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              height: 1.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          // bullet point
+                          Icon(
+                            Icons.tips_and_updates,
+                            size: 22,
+                            color: Color.fromARGB(255, 220, 104, 145),
+                          ),
+                          SizedBox.square(
+                            dimension: 10,
+                          ),
+                          //   wrapped text
+                          Text(
+                            'Fill up on Folic acid.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              height: 1.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ],
+                )),
           ),
         ],
-      ),
+      )),
     );
   }
 }
